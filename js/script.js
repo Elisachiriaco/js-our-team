4e2901e00cd4602515b379980b3522bf6ac859f4
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-04-07 15:40:01 +0200

    array di oggetti

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+
+const membri = [
+    {
+        nome: 'Wayne',
+        cognome: 'Barnett',
+        ruolo: 'Founder & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome: 'Angela',
+        cognome: 'Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        nome: 'Walter',
+        cognome: 'Gordon',
+        ruolo: 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        nome: 'Angela',
+        cognome: 'Lopez',
+        ruolo: 'Social Media Manager',
+        foto:'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome: 'Scott',
+        cognome: 'Estrada',
+        ruolo: 'Developer',
+        foto: 'scott-estrada-developer.jpg'
+    },
+    {
+        nome: 'Barbara',
+        cognome: 'Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
